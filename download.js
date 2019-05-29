f723d04db57fec00f738490f114a0402b4c1db18
albanjin <albanjin@sina.com>
2019-05-29 17:16:01 +0800

    update

--- download.js
+++ download.js
@@ -5,7 +5,9 @@ async function upload(cmdStr) {
     console.log('stdout:', stdout)
     console.log('stderr:', stderr)
 }
-upload(` scp root@118.31.23.219:/home/tomcat/webapps/dist/test.html /Users/albanjin/Downloads/UcShare/`)
+// upload(` scp root@118.31.23.219:/home/tomcat/webapps/dist/test.html /Users/albanjin/Downloads/UcShare/`)
+// /Users/albanjin/Downloads/UcShare/
+upload(`scp -rv  /Users/albanjin/Downloads/UcShare/test/view   root@118.31.23.219:/home/tomcat/webapps/`)
 
 
 
